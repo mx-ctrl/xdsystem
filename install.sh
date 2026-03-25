2dc93dd9efb97dcab5bd75a4cc51fccc9ecd20b3
mx-ctrl <mansonpetrini145@gmail.com>
2026-03-25 15:52:21 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,26 @@
-# Kasih izin dan jalankan
+#!/data/data/com.termux/files/usr/bin/bash
+
+# ==============================
+# ENZOX INSTALLER
+# ==============================
+
+GREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+CYAN='\033[1;36m'
+RESET='\033[0m'
+
+clear
+echo -e "${CYAN}======================================${RESET}"
+echo -e "${GREEN}        ENZOX INSTALLER${RESET}"
+echo -e "${CYAN}======================================${RESET}"
+echo ""
+
+echo -e "${YELLOW}Downloading ENZOX...${RESET}"
+curl -L "https://github.com/mx-ctrl/xdsystem/releases/download/latest/enzox" -o enzox
+
+echo -e "${YELLOW}Setting permission...${RESET}"
+chmod +x enzox
+
 echo -e "${YELLOW}Starting ENZOX...${RESET}"
 echo ""
-chmod +x "$BINARY_NAME"
-exec ./"$BINARY_NAME"
+exec ./enzox
